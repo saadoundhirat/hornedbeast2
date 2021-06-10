3e25e2a7e355b0c5124602b896f204abea08b92f
saadoundhirat <saadoundhirat93@gmail.com>
2021-06-10 15:15:25 +0300

    adding sort method

--- src/components/FormSelect.js
+++ src/components/FormSelect.js
@@ -7,9 +7,17 @@ class FormSelect extends React.Component {
   updateFormSelectValue = (event) => {
     event.preventDefault();
     let selectValue = event.target.value;
-    // console.log(selectValue);
     this.props.renderBasedOnSelectValue(selectValue);
   };
+
+  // 😴😴😴 call sort object 
+  sort = (event) => {
+    event.preventDefault();
+    let selectValue = event.target.value;
+    this.props.handleSort(selectValue);
+  };
+
+  
 
   //render form
   render() {
@@ -31,6 +39,20 @@ class FormSelect extends React.Component {
                 <option value="3">three</option>
                 <option value="100">wow</option>
               </Form.Control>
+              {/* use the sort method to sort items based on the # of hornS Or A-Z */}
+              <Form.Label>
+                Sort Horns
+              </Form.Label>
+              <Form.Control 
+               as="select" 
+               custom
+               id="disabledSelect" 
+               onChange={this.sort}>
+                <option value="default">Default</option>
+                <option value="horns">horn numbers</option>
+                <option value="A-Z">from A-Z</option>
+              </Form.Control>
+              
             </Form.Group>
         </Form>
       </div>
